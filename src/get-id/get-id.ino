a6482b0d6c0ff8c97d249d5cd31fd9a9efa6973c
vaaandark <vaaandark@gmail.com>
2022-03-29 18:43:28 +0800

    make the log format better

--- src/get-id/get-id.ino
+++ src/get-id/get-id.ino
@@ -3,10 +3,11 @@
 
 #define CHIP_SELECTION_PIN 10
 #define RESET_POWER_DOWN_PIN 9
+#define UID_SIZE 4
 
 MFRC522 rfid(CHIP_SELECTION_PIN, RESET_POWER_DOWN_PIN);
 
-byte uid[4];
+byte uid[UID_SIZE];
 
 void setup() {
 	Serial.begin(9600);
@@ -14,19 +15,21 @@ void setup() {
 	rfid.PCD_Init();
 }
 
-void printID(byte *uid_arr, byte size, int base)
+void printID(byte *uid, byte size, int base)
 {
-	for (byte i = 0; i < size; ++i)
-		Serial.print(uid_arr[i], base);
+	for (byte i = 0; i < size; ++i) {
+		Serial.print(uid[i], base);
+		Serial.print(' ');
+	}
 	Serial.println();
 }
 
 void loop() {
 	if (rfid.PICC_IsNewCardPresent() && rfid.PICC_ReadCardSerial()) {
-		for (byte i = 0; i < 4; ++i)
+		for (byte i = 0; i < UID_SIZE; ++i)
 			uid[i] = rfid.uid.uidByte[i];
 
-		Serial.print("UID (hex) :");
+		Serial.print("UID (hex) : ");
 		printID(rfid.uid.uidByte, rfid.uid.size, HEX);
 
 		Serial.print("UID (dec) : ");
